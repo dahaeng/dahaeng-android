5a25c476a69eac6a8d292b3e0c8bd97c7e4ff23e
jisungbin <ji@sungb.in>
2022-01-06 00:08:33 +0900

    Set exoplayer to background login activity

--- presentation/src/main/kotlin/team/dahaeng/android/activity/login/LoginActivity.kt
+++ presentation/src/main/kotlin/team/dahaeng/android/activity/login/LoginActivity.kt
@@ -9,9 +9,12 @@
 
 package team.dahaeng.android.activity.login
 
+import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.viewModels
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 import team.dahaeng.android.R
 import team.dahaeng.android.activity.base.BaseActivity
 import team.dahaeng.android.databinding.ActivityLoginBinding
@@ -20,12 +23,22 @@ import team.dahaeng.android.secret.Resources
 class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(R.layout.activity_login) {
 
     override val vm: LoginViewModel by viewModels()
-
     private var player: ExoPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+    }
 
     private fun initExoPlayer() {
         player = ExoPlayer.Builder(this)
             .build()
+            .apply {
+                repeatMode = Player.REPEAT_MODE_ONE
+            }
             .also { player ->
                 val video = MediaItem.fromUri(Resources.Intro)
                 player.setMediaItem(video)
