08e3315f1b3eed782e3cbdc9dad1530f6521b706
Ji Sungbin <ji@sungb.in>
2022-01-14 19:07:36 +0900

    remove duplicate copyright

--- domain/src/main/kotlin/team/dahaeng/android/domain/community/model/travel/Theme.kt
+++ domain/src/main/kotlin/team/dahaeng/android/domain/community/model/travel/Theme.kt
@@ -2,15 +2,6 @@
  * Dahaeng © 2022 Ji Sungbin, 210202. all rights reserved.
  * Dahaeng license is under the MIT.
  *
- * [Theme.kt] created by Ji Sungbin on 22. 1. 13. 오후 6:47
- *
- * Please see: https://github.com/dahaeng/dahaeng-android/blob/main/LICENSE.
- */
-
-/*
- * Dahaeng © 2022 Ji Sungbin, 210202. all rights reserved.
- * Dahaeng license is under the MIT.
- *
  * [Theme.kt] created by Ji Sungbin on 22. 1. 13. 오후 6:44
  *
  * Please see: https://github.com/dahaeng/dahaeng-android/blob/main/LICENSE.
