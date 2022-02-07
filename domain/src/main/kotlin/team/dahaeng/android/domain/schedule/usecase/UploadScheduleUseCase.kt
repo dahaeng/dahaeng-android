3866b723cea44cab15a1579cd47737ebf432ce77
jisungbin <ji@sungb.in>
2022-02-07 14:28:26 +0900

    Update ui

--- domain/src/main/kotlin/team/dahaeng/android/domain/schedule/usecase/UploadScheduleUseCase.kt
+++ domain/src/main/kotlin/team/dahaeng/android/domain/schedule/usecase/UploadScheduleUseCase.kt
@@ -4,7 +4,7 @@ import team.dahaeng.android.domain.community.repository.FirebaseRepository
 import team.dahaeng.android.domain.schedule.model.Schedule
 
 class UploadScheduleUseCase(private val repository: FirebaseRepository) {
-    suspend operator fun invoke(schedule: Schedule, id: String) = runCatching {
-        repository.uploadSchedule(schedule, id)
+    suspend operator fun invoke(schedule: Schedule) = runCatching {
+        repository.uploadSchedule(schedule)
     }
 }
