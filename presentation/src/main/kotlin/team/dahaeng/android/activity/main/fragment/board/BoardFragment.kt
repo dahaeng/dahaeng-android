d06f11971f6b62424df9d9c1324c5dde63655429
jisungbin <ji@sungb.in>
2022-03-05 04:28:44 +0900

    Add Deprecated annotation to Location.tryParseAddress() function

--- presentation/src/main/kotlin/team/dahaeng/android/activity/main/fragment/board/BoardFragment.kt
+++ presentation/src/main/kotlin/team/dahaeng/android/activity/main/fragment/board/BoardFragment.kt
@@ -16,12 +16,12 @@ import android.view.View
 import androidx.fragment.app.activityViewModels
 import com.birjuvachhani.locus.Locus
 import io.github.jisungbin.logeukes.logeukes
+import java.util.Locale
 import team.dahaeng.android.R
 import team.dahaeng.android.activity.base.BaseFragment
 import team.dahaeng.android.activity.main.MainViewModel
 import team.dahaeng.android.data.DataStore
 import team.dahaeng.android.databinding.FragmentListBinding
-import java.util.Locale
 
 class BoardFragment : BaseFragment<FragmentListBinding, MainViewModel>(R.layout.fragment_list) {
 
@@ -31,11 +31,7 @@ class BoardFragment : BaseFragment<FragmentListBinding, MainViewModel>(R.layout.
         super.onViewCreated(view, savedInstanceState)
 
         if (vm.lastLocate.isEmpty()) {
-            // getCurrentLocate 하면 위치 권한이 없을 때도 딱 한 번만 실행되서
-            // Error 로 위치가 받아와 짐
-            // 따라서 지속적으로 위치를 받고, 한 번 제대로 들어오면(권한이 다 수락 돼서)
-            // 이후 위치 업데이트를 중단함
-            Locus.startLocationUpdates(this) { result ->
+            Locus.getCurrentLocation(requireContext()) { result ->
                 result.location?.let { location ->
                     location.tryParseAddress()?.let { address ->
                         vm.lastLocate = address
@@ -65,6 +61,7 @@ class BoardFragment : BaseFragment<FragmentListBinding, MainViewModel>(R.layout.
         }
     }
 
+    @Deprecated(message = "Use reverse geocoding api from t-map instead of.")
     private fun Location.tryParseAddress() = try {
         val geoCoder = Geocoder(requireContext().applicationContext, Locale.KOREA)
         val addressList = geoCoder.getFromLocation(latitude, longitude, 2)
