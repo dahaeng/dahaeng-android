6b78748ae47a89c8e42d1ffb07f0fe0db70e1726
jisungbin <ji@sungb.in>
2022-03-05 04:55:12 +0900

    Update User model

--- domain/src/main/kotlin/team/dahaeng/android/domain/aouth/model/User.kt
+++ domain/src/main/kotlin/team/dahaeng/android/domain/aouth/model/User.kt
@@ -12,8 +12,19 @@ package team.dahaeng.android.domain.aouth.model
 import android.graphics.drawable.ColorDrawable
 import kotlin.random.Random
 
-// profileImageUrl: String or ColorDrawable
-data class User(val id: Long, val nickname: String, val profileImageUrl: Any) {
+/**
+ * 내 정보 객체
+ *
+ * @property id 유저 UUID
+ * @property nickname 유저 닉네임
+ * @property profileImageUrl 프로필 사진을 나타내기 위한 필드임
+ * String(프로필 이미지 주소) or ColorDrawable(단일 색) 이 될 수 있음
+ */
+data class User(
+    val id: Long,
+    val nickname: String,
+    val profileImageUrl: Any,
+) {
     companion object {
         private val randomColor get() = (Math.random() * 16777215).toInt() or (0xFF shl 24)
         fun getDefaultNickname() = "사용자${Random.nextInt(1000, 10000)}"
