c84775374a8c7c867200a99a3d8bc9a1eee1cada
jisungbin <ji@sungb.in>
2022-03-05 13:25:56 +0900

    Update models

--- domain/src/main/kotlin/team/dahaeng/android/domain/aouth/model/User.kt
+++ domain/src/main/kotlin/team/dahaeng/android/domain/aouth/model/User.kt
@@ -19,14 +19,22 @@ import kotlin.random.Random
  * @property nickname 유저 닉네임
  * @property profileImageSrc 프로필 사진을 나타내기 위한 필드임
  * String(프로필 이미지 주소) or ColorDrawable(단일 색) 이 될 수 있음
+ * @property likeScheduleIds 좋아요한 일정 아이디들
+ * @property joinScheduleIds 참여한 일정 아이디들
+ * @property bedges 휙득한 벳지 아이디들
+ * @property verifyScehdules 여행 실제 진행이 인증된 일정 아이디들
  */
 data class User(
     val id: Long = Random.nextLong(),
     val nickname: String = randomNickname,
     val profileImageSrc: Any = randomColorDrawable,
+    val likeScheduleIds: List<Long> = emptyList(),
+    val joinScheduleIds: List<Long> = emptyList(),
+    val bedges: List<Int> = emptyList(),
+    val verifyScehdules: List<Long> = emptyList(),
 ) {
     private companion object {
         val randomColorDrawable get() = ColorDrawable((Math.random() * 16777215).toInt() or (0xFF shl 24))
-        val randomNickname get() = "사용자${Random.nextInt(1000, 10000)}"
+        val randomNickname get() = "J${Random.nextInt(1000, 10000)}"
     }
 }
--- domain/src/main/kotlin/team/dahaeng/android/domain/schedule/model/LastStep.kt
+++ domain/src/main/kotlin/team/dahaeng/android/domain/schedule/model/LastStep.kt
@@ -9,11 +9,15 @@
 
 package team.dahaeng.android.domain.schedule.model
 
+import team.dahaeng.android.domain.schedule.constant.Step
+
 /**
+ * 진행중인 일정 복구에 필요한 정보들을 가지고 있는 객체
+ *
  * @property level 마지막으로 머무른 일정 단계
  * @property startAt [level] 단계에서 투표가 시작된 시간
  */
 data class LastStep(
-    val level: Int = 0,
+    val level: Int = Step.Theme,
     val startAt: Long = 0L,
 )
