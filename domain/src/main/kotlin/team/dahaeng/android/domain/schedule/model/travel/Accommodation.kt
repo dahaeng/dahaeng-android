0924a4d6bc3244dc47ba31e2ca98b515109e1d3c
jisungbin <ji@sungb.in>
2022-03-08 23:11:35 +0900

    Update model

--- domain/src/main/kotlin/team/dahaeng/android/domain/schedule/model/travel/Accommodation.kt
+++ domain/src/main/kotlin/team/dahaeng/android/domain/schedule/model/travel/Accommodation.kt
@@ -9,16 +9,18 @@
 
 package team.dahaeng.android.domain.schedule.model.travel
 
-import team.dahaeng.android.domain.util.constant.EmptyString
 import java.io.Serializable
+import team.dahaeng.android.domain.util.constant.EmptyString
 
 /**
  * 숙소 객체
  *
  * @property name 숙소 이름
+ * @property description 숙소 설명
  * @property locate 숙소 위치
  */
 data class Accommodation(
     val name: String = EmptyString,
+    val description: String = EmptyString,
     val locate: Locate = Locate(),
 ) : Serializable
